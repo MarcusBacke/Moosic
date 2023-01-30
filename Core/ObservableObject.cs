e57cfc479a5e4732e39bf224f2647d6f61b377de
Ricardo Prins <ricardoprins@gmail.com>
2023-01-29 17:19:41 -0700

    Add basic structure

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Moosic.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+
+#pragma warning disable CS8632
+        public event PropertyChangedEventHandler? PropertyChanged;
+#pragma warning restore CS8632
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
