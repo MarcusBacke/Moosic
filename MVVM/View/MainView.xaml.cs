57b7b14c10de4696d7b43c63f0b470d3f64b2321
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-03-06 14:55:02 -0700

    added a local file explorer
    there are some other errors that won't allow it to work but I've tested the code and know it will work outside

--- MVVM/View/MainView.xaml.cs
+++ MVVM/View/MainView.xaml.cs
@@ -21,5 +21,23 @@ namespace Moosic.MVVM.View
     public partial class MainView : UserControl
     {
         public MainView() => InitializeComponent();
+
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
+        public MediaPlayerAudioSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenAudioFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                mediaPlayer.Play();
+            }
+        }   
     }
 }
